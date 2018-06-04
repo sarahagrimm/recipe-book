f2667353c0f9666801b3b1d9bcec82700f1ca4c2
Pratima Sakinala <pratimasakinala@Pratimas-MacBook-Pro.local>
2018-06-04 16:50:01 -0700

    creates shopping list feature

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,12 @@
+export class Ingredient {
+    // public name: string;
+    // public amount: number;
+
+    // constructor(name: string, amount: number) {
+    //     this.name = name;
+    //     this.amount = amount;
+    // }
+
+    // simple way to setup a class definition:
+    constructor(public name: string, public amount: number) {}
+}
