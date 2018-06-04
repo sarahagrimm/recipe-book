fa7121e24bbbc636b6c2d791a96ac559f3abe6c1
Sarah Grimm <sarahagrimm@gmail.com>
2018-06-04 16:50:06 -0700

    creates shopping list feature

---
+++ src/app/Shared/ingredient.model.ts
@@ -0,0 +1,12 @@
+export class Ingredient {
+    // public name: string;
+    // public amount: number;
+
+    // constructor(name: string, amount: number){
+    //     this.name = name;
+    //     this.amount = amount;
+    // }
+
+    // Simple way to setup a class definition: 
+    constructor(public name: string, public amount: number) {}
+}
