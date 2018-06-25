e2228d365e29d6369a6856e98150a90355f4e8fc
Sarah Grimm <sarahagrimm@gmail.com>
2018-06-25 11:21:22 -0700

    adding ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,17 +1,31 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     private recipes: Recipe[] = [
         {
             name: 'A new recipe',
             description: 'test recipe',
-            imagePath: 'https://www.ndtv.com/cooks/images/chicken-chettinad_620.jpg'
+            imagePath: 'https://www.ndtv.com/cooks/images/chicken-chettinad_620.jpg',
+            ingredients: [
+                {
+                    name: 'Meat',
+                    amount: 1
+                },
+                new Ingredient('Tomatoes', 5)
+            ]
         },
         new Recipe('A Test Recipe',
             'This is a simple test.',
-            'https://www.sbs.com.au/food/sites/sbs.com.au.food/files/IMG_1105.jpg')
+            'https://www.sbs.com.au/food/sites/sbs.com.au.food/files/IMG_1105.jpg',
+        [
+            new Ingredient('Meat', 1),
+            new Ingredient('Coconut Milk', 1)
+            
+        ]
+        )
     ];
 
     recipeSelected = new EventEmitter<Recipe>();
